27b4762b63042322f59b3961803b13186dbe878d
americorodrigues25 <americo.200422@gmail..com>
2025-08-22 18:45:08 -0300

    Andamento do projeto dev blog

--- 10-DEVBLOG/dev-blog/src/pages/Home/Home.jsx
+++ 10-DEVBLOG/dev-blog/src/pages/Home/Home.jsx
@@ -1,12 +1,44 @@
 // css
-import styles from './Home.module.css';
+import styles from "./Home.module.css";
+
+// hooks
+import { useNavigate, Link } from "react-router-dom";
+import { useState } from "react";
+
+// components
+
 
 const Home = () => {
-    return (
-        <div>
-            Home
-        </div>
-    )
-}
+  const [query, setQuery] = useState("");
+  const [posts] = useState([]);
 
-export default Home
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className={styles.home}>
+      <h1>Veja nossos psts mais recentes</h1>
+      <form onSubmit={handleSubmit} className={styles.search_form}>
+        <input
+          type="text"
+          placeholder="Ou busque por tags..."
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="btn btn_dark">Pesquisar</button>
+      </form>
+      <div>
+        <h2>Posts...</h2>
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts</p>
+            <Link to="/posts/create" className="btn">
+              Criar primeiro post
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
