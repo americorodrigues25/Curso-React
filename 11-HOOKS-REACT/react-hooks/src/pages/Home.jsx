3e8ba30b10750109e5c32ac24934d6834a2eb57c
americorodrigues25 <americo.200422@gmail.com>
2025-08-27 21:26:20 -0300

    Finalização da sessão 11 - React Hooks

--- 11-HOOKS-REACT/react-hooks/src/pages/Home.jsx
+++ 11-HOOKS-REACT/react-hooks/src/pages/Home.jsx
@@ -6,6 +6,10 @@ import HookUseState from "../components/HookUseState";
 import HookUseReducer from "../components/HookUseReducer";
 import HookUseEffect from "../components/HookUseEffect";
 import HookUseRef from "../components/HookUseRef";
+import HookUseCallback from "../components/HookUseCallback";
+import HookUseMemo from "../components/HookUseMemo";
+import HookUseLayoutEffect from "../components/HookUseLayoutEffect";
+import HookUseImperativeHandle from "../components/HookUseImperativeHandle";
 
 
 const Home = () => {
@@ -20,6 +24,10 @@ const Home = () => {
       <p>Valor do contexto: {contextValue}</p>
       <hr />
       <HookUseRef />
+      <HookUseCallback />
+      <HookUseMemo />
+      <HookUseLayoutEffect />
+      <HookUseImperativeHandle />
     </div>
   );
 };
