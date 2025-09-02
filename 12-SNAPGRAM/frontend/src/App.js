be731b255b7a894b9dfc0dfc58931e2bbf40b041
americorodrigues25 <americo.200422@gmail..com>
2025-09-02 17:15:26 -0300

    Continuação do projeto snapgram - integração do frontend com backend

--- 12-SNAPGRAM/frontend/src/App.js
+++ 12-SNAPGRAM/frontend/src/App.js
@@ -3,6 +3,10 @@ import "./App.css";
 // router
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
+// components
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
 // pages
 import Home from "./pages/Home/Home";
 import Login from "./pages/Auth/Login";
@@ -12,11 +16,15 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+        <Footer />
       </BrowserRouter>
     </div>
   );
--- 12-SNAPGRAM/frontend/src/pages/Auth/Register.jsx
+++ 12-SNAPGRAM/frontend/src/pages/Auth/Register.jsx
@@ -1,11 +1,66 @@
-import React from 'react'
+import "./Auth.css";
+
+// components
+import { Link } from "react-router-dom";
+
+// hooks
+import { useState, useEffect } from "react";
 
 const Register = () => {
-  return (
-    <div>
-      register
-    </div>
-  )
-}
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-export default Register
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const user = {
+      name,
+      email,
+      password,
+      confirmPassword,
+    };
+
+    console.log(user);
+  };
+
+  return (
+    <div id="register">
+      <h2>SnapGram</h2>
+      <p className="subtitle">cadastre-se para ver as fotos de seus amigos.</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Nome"
+          onChange={(e) => setName(e.target.value)}
+          value={name || "" }
+        />
+        <input
+          type="email"
+          placeholder="E-mail"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email || "" }
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password || "" }
+        />
+        <input
+          type="password"
+          placeholder="Confirme a senha"
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          value={confirmPassword || "" }
+        />
+        <input type="submit" value="Cadastrar" />
+      </form>
+      <p>
+        Já possui uma conta ? <Link to="/login">Clique aqui.</Link>
+      </p>
+    </div>
+  );
+};
+
+export default Register;
