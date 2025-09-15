97ae394bbbbb330af0951c4e9fe6ba564f2401e6
americorodrigues25 <americo.200422@gmail.com>
2025-09-15 20:58:50 -0300

    Continuação do curso de react - introdução ao TS

---
+++ 16-SETUP-TS/index.js
@@ -0,0 +1,7 @@
+var firstName = "Americo";
+var anotherName = 1;
+var x = true;
+function greeting(name) {
+    console.log("Ola, " + name);
+}
+greeting(firstName);
---
+++ 16-SETUP-TS/index.ts
@@ -0,0 +1,9 @@
+const firstName = "Americo";
+const anotherName = 1;
+const x = true;
+
+function greeting(name: string) {
+  console.log("Ola, " + name);
+}
+
+greeting(firstName)
