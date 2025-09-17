411973f463b23c876580c053da825f1497f3d64c
americorodrigues25 <americo.200422@gmail..com>
2025-09-17 17:18:41 -0300

    Finalizando curso de react

--- 18-todo-react-com-ts/src/components/Footer.tsx
+++ 18-todo-react-com-ts/src/components/Footer.tsx
@@ -2,14 +2,13 @@ import React from "react";
 import styles from "./Footer.module.css";
 
 const Footer = () => {
-    return (
-        <footer className={styles.footer}>
-            <p>
-                <span>React + TS Todo</span>
-                @ 2025
-            </p>
-        </footer>
-    )
-}
+  return (
+    <footer className={styles.footer}>
+      <p>
+        <span>React + TS Todo</span> @2025
+      </p>
+    </footer>
+  );
+};
 
 export default Footer;
