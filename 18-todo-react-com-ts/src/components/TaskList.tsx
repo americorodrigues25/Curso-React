300bae0093bb16b904b7379d65b04aed7b2f80a2
americorodrigues25 <americo.200422@gmail..com>
2025-09-18 17:27:12 -0300

    Finalizando curso de react

--- 18-todo-react-com-ts/src/components/TaskList.tsx
+++ 18-todo-react-com-ts/src/components/TaskList.tsx
@@ -1,15 +1,32 @@
 import React from "react";
+import { ITask } from "../interfaces/Taks";
+import styles from "./TaskList.module.css";
 
 type Props = {
+  taskList: ITask[];
+};
 
-}
-
-const TaskList = (props: Props) => {
-    return (
-        <div>
-            lista
-        </div>
-    )
-}
+const TaskList = ({ taskList }: Props) => {
+  return (
+    <>
+      {taskList.length > 0 ? (
+        taskList.map((task) => (
+          <div key={task.id} className={styles.task}>
+            <div className={styles.details}>
+            <h4>{task.title}</h4>
+            <p>Dificuldade: {task.difficulty}</p>
+            </div>
+            <div className={styles.actions}>
+                <i className="bi bi-pencil"></i>
+                <i className="bi bi-trash"></i>
+            </div>
+          </div>
+        ))
+      ) : (
+        <p>Não há tarefas cadastradas.</p>
+      )}
+    </>
+  );
+};
 
 export default TaskList;
